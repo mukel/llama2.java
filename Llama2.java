0e3768dc9364355e34289022880005f4df2344e6
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2023-09-04 01:24:21 +0200

    Sync with llama2.c .

--- Llama2.java
+++ Llama2.java
@@ -13,7 +13,9 @@ import jdk.incubator.vector.FloatVector;
 import jdk.incubator.vector.VectorOperators;
 import jdk.incubator.vector.VectorSpecies;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.SegmentScope;
 import java.nio.ByteBuffer;
@@ -23,11 +25,7 @@ import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.SortedMap;
+import java.util.*;
 import java.util.stream.IntStream;
 
 final class Config {
@@ -146,8 +144,8 @@ final class RunState {
     final float[] att; // buffer for scores/attention values (n_heads, seq_len)
     final float[] logits; // output logits
     // kv cache
-    final float[] key_cache;   // (layer, seq_len, dim)
-    final float[] value_cache; // (layer, seq_len, dim)
+    final float[][] key_cache;   // (layer, seq_len, dim)
+    final float[][] value_cache; // (layer, seq_len, dim)
 
     RunState(Config config) {
         int kv_dim = (config.dim * config.n_kv_heads) / config.n_heads;
@@ -161,8 +159,8 @@ final class RunState {
         this.v = new float[kv_dim];
         this.att = new float[config.n_heads * config.seq_len];
         this.logits = new float[config.vocab_size];
-        this.key_cache = new float[config.n_layers * config.seq_len * kv_dim];
-        this.value_cache = new float[config.n_layers * config.seq_len * kv_dim];
+        this.key_cache = new float[config.n_layers][config.seq_len * kv_dim];
+        this.value_cache = new float[config.n_layers][config.seq_len * kv_dim];
     }
 }
 
@@ -386,9 +384,12 @@ class Llama2 {
             }
 
             // save key,value at this time step (pos) to our kv cache
-            int loff = l * p.seq_len * kv_dim; // kv cache layer offset for convenience
-            System.arraycopy(s.k, 0, s.key_cache, loff + pos * kv_dim, kv_dim);
-            System.arraycopy(s.v, 0, s.value_cache, loff + pos * kv_dim, kv_dim);
+            //int loff = l * p.seq_len * kv_dim; // kv cache layer offset for convenience
+            System.arraycopy(s.k, 0, s.key_cache[l], pos * kv_dim, kv_dim);
+            System.arraycopy(s.v, 0, s.value_cache[l], pos * kv_dim, kv_dim);
+
+
+            final int curLayer = l;
 
             // multihead attention. iterate over all heads
             IntStream.range(0, p.n_heads).parallel().forEach(h -> {
@@ -404,11 +405,11 @@ class Llama2 {
                 for (int t = 0; t <= pos; t++) {
                     // get the key vector for this head and at this timestep
                     // float* k = s->key_cache + loff + t * kv_dim + (h / kv_mul) * head_size;
-                    int keyCacheOffset = loff + t * kv_dim + (h / kv_mul) * head_size;
+                    int keyCacheOffset = t * kv_dim + (h / kv_mul) * head_size;
                     // calculate the attention score as the dot product of q and k
                     float score = 0.0f;
                     for (int i = 0; i < head_size; i++) {
-                        score += s.q[qOffset + i] * s.key_cache[keyCacheOffset + i];
+                        score += s.q[qOffset + i] * s.key_cache[curLayer][keyCacheOffset + i];
                     }
                     score /= (float) Math.sqrt(head_size);
                     // save the score to the attention buffer
@@ -427,12 +428,12 @@ class Llama2 {
                 for (int t = 0; t <= pos; t++) {
                     // get the value vector for this head and at this timestep
                     // float* v = s->value_cache + loff + t * kv_dim + (h / kv_mul) * head_size;
-                    int vOffset = loff + t * kv_dim + (h / kv_mul) * head_size;
+                    int vOffset = t * kv_dim + (h / kv_mul) * head_size;
                     // get the attention weight for this timestep
                     float a = s.att[attOffset + t];
                     // accumulate the weighted value inconfigto xb
                     for (int i = 0; i < head_size; i++) {
-                        s.xb[xbOffset + i] += a * s.value_cache[vOffset + i];
+                        s.xb[xbOffset + i] += a * s.value_cache[curLayer][vOffset + i];
                     }
                 }
             });
@@ -632,6 +633,11 @@ class Llama2 {
 // generation loop
 
     static void generate(Transformer transformer, Tokenizer tokenizer, Sampler sampler, String prompt, int steps) {
+        String empty_prompt = "";
+        if (prompt == null) {
+            prompt = empty_prompt;
+        }
+
         // encode the (string) prompt into tokens sequence
         int num_prompt_tokens = 0; // the total number of prompt tokens
         int[] prompt_tokens = new int[prompt.length() * 2 + 3]; // +3 for '\0', ?BOS, ?EOS
@@ -818,6 +824,107 @@ class Llama2 {
         return next;
     }
 
+    static String read_stdin(String guide) {
+        // read a line from stdin, up to but not including \n
+        System.out.print(guide);
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextLine()) {
+            return scanner.nextLine();
+        }
+        return null;
+    }
+
+// ----------------------------------------------------------------------------
+// chat loop
+// I manually inspected the tokens for a few chat conversations compared to
+// python reference and that seemed ok, but this was not thoroughly tested and
+// is not safely implemented, it's more a proof of concept atm.
+
+    static void chat(Transformer transformer, Tokenizer tokenizer, Sampler sampler,
+              String cli_user_prompt, String cli_system_prompt, int steps) {
+
+        // buffers for reading the system prompt and user prompt from stdin
+        String system_prompt = null;
+        String user_prompt = null;
+        String rendered_prompt = null;
+        int num_prompt_tokens = 0;
+        int[] prompt_tokens = new int[512];
+        int user_idx = 0;
+
+        // start the main loop
+        boolean user_turn = true; // user starts
+        int next = 0;        // will store the next token in the sequence
+        int token = 0;       // stores the current token to feed into the transformer
+        int prev_token;
+        int pos = 0;     // position in the sequence
+        while (pos < steps) {
+
+            // when it is the user's turn to contribute tokens to the dialog...
+            if (user_turn) {
+                // get the (optional) system prompt at position 0
+                if (pos == 0) {
+                    // at position 0, the user can also contribute a system prompt
+                    if (cli_system_prompt == null) {
+                        // system prompt was not passed in, attempt to get it from stdin
+                        system_prompt = read_stdin("Enter system prompt (optional): ");
+                    } else {
+                        // system prompt was passed in, use it
+                        system_prompt = cli_system_prompt;
+                    }
+                }
+                // get the user prompt
+                if (pos == 0 && cli_user_prompt != null) {
+                    // user prompt for position 0 was passed in, use it
+                    user_prompt = cli_user_prompt;
+                } else {
+                    // otherwise get user prompt from stdin
+                    user_prompt = read_stdin("User: ");
+                }
+                // render user/system prompts into the Llama 2 Chat schema
+                if (pos == 0 && system_prompt.isEmpty()) {
+                    String system_template = "[INST] <<SYS>>\n%s\n<</SYS>>\n\n%s [/INST]";
+                    rendered_prompt = system_template.formatted(system_prompt, user_prompt);
+                } else {
+                    String user_template = "[INST] %s [/INST]";
+                    rendered_prompt = user_template.formatted(user_prompt);
+                }
+                // encode the rendered prompt into tokens
+                num_prompt_tokens = encode(tokenizer, rendered_prompt, true, false, prompt_tokens);
+                user_idx = 0; // reset the user index
+                user_turn = false;
+                System.out.print("Assistant: ");
+            }
+
+            // determine the token to pass into the transformer next
+            if (user_idx < num_prompt_tokens) {
+                // if we are still processing the input prompt, force the next prompt token
+                token = prompt_tokens[user_idx++];
+            } else {
+                // otherwise use the next token sampled from previous turn
+                token = next;
+            }
+            // EOS (=2) token ends the Assistant turn
+            if (token == 2) {
+                user_turn = true;
+            }
+
+            // forward the transformer to get logits for the next token
+            float[] logits = forward(transformer, token, pos);
+            next = sample(sampler, logits);
+            pos++;
+
+            if (user_idx >= num_prompt_tokens && next != 2) {
+                // the Assistant is responding, so print its output
+                String piece = decode(tokenizer, token, next);
+                safe_printf(piece); // same as printf("%s", piece), but skips "unsafe" bytes
+                System.out.flush();
+            }
+            if (next == 2) {
+                System.out.println();
+            }
+        }
+        System.out.println();
+    }
 
 
 // ----------------------------------------------------------------------------
@@ -833,6 +940,8 @@ class Llama2 {
         System.err.println("  -n <int>    number of steps to run for, default 256. 0 = max_seq_len");
         System.err.println("  -i <string> input prompt");
         System.err.println("  -z <string> optional path to custom tokenizer");
+        System.err.println("  -m <string> mode: generate|chat, default: generate");
+        System.err.println("  -y <string> (optional) system prompt in chat mode");
         System.exit(1);
     }
 
@@ -840,11 +949,13 @@ class Llama2 {
         // default parameters
         String checkpoint_path = null; // e.g. out/model.bin
         String tokenizer_path = "tokenizer.bin";
-        float temperature = 1.0f; // 0.0 = greedy deterministic. 1.0 = original. don't set higher
-        float topp = 0.9f;        // top-p in nucleus sampling. 1.0 = off. 0.9 works well, but slower
-        long rng_seed = 0;        // seed rng with time by default
-        int steps = 256;          // max number of steps to run for, 0: use seq_len
-        String prompt = "";       // prompt string
+        float temperature = 1.0f;    // 0.0 = greedy deterministic. 1.0 = original. don't set higher
+        float topp = 0.9f;           // top-p in nucleus sampling. 1.0 = off. 0.9 works well, but slower
+        long rng_seed = 0;           // seed rng with time by default
+        int steps = 256;             // max number of steps to run for, 0: use seq_len
+        String prompt = null;        // prompt string
+        String mode = "generate";    // generate|chat
+        String system_prompt = null; // the (optional) system prompt to use in chat mode
 
         // poor man's C argparse so we can override the defaults above from the command line
         if (args.length >= 1) {
@@ -865,6 +976,8 @@ class Llama2 {
                 case 'n' -> steps = Integer.parseInt(args[i + 1]);
                 case 'i' -> prompt = args[i + 1];
                 case 'z' -> tokenizer_path = args[i + 1];
+                case 'm' -> mode = args[i + 1];
+                case 'y' -> system_prompt = args[i + 1];
                 default -> error_usage();
             }
         }
@@ -896,6 +1009,13 @@ class Llama2 {
         Sampler sampler = new Sampler(transformer.config.vocab_size, temperature, topp, rng_seed);
 
         // run!
-        generate(transformer, tokenizer, sampler, prompt, steps);
+        switch (mode) {
+            case "generate" -> generate(transformer, tokenizer, sampler, prompt, steps);
+            case "chat" -> chat(transformer, tokenizer, sampler, prompt, system_prompt, steps);
+            default -> {
+                System.err.println("unknown mode: " + mode);
+                error_usage();
+            }
+        }
     }
 }
